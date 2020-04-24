eddb17c662936e6155f0b64dd6000d10849d2207
clintwallace66 <64282747+clintwallace66@users.noreply.github.com>
2020-04-24 15:34:12 -0500

    Add files via upload

---
+++ movielist_final/Movie.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author clint
+ */
+/*
+ * Movie class
+ */
+package movielist_final;
+
+public class Movie {
+    private String movieTitle;
+    private String rating;
+    private int year;
+    private int runTime;
+    public Movie(String title, String rating, int year, int runTime)
+    {
+        this.movieTitle = title;
+        this.rating = rating;
+        this.year = year;
+        this.runTime = runTime;
+    }
+    public String getMovieTitle()
+    {
+        return movieTitle;
+    }
+    public void setMovieTitle(String value)
+    {
+        movieTitle = value;
+    }
+    public String getRating()
+    {
+        return rating;
+    }
+    public void setRating(String value)
+    {
+        rating = value;
+    }
+    public int getYear()
+    {
+        return year;
+    }
+    public void setYear(int value)
+    {
+        year = value;
+    }
+    public int getRunTime()
+    {
+        return runTime;
+    }
+    public void setRunTime(int value)
+    {
+        runTime = value;
+    }
+    public String toString()
+    {
+        return "Movie title: " + movieTitle + 
+                "\n Rating: " + rating + 
+                "\n Year: " + year + 
+                "\n Total runtime: " + runTime;
+    }
+    
+        
+}
